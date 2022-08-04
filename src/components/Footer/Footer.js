b3cf3d032059e6f93c56a0c366274134dcba95d4
chanpi23 <chandrikad34@gmail.com>
2022-08-03 21:48:21 -0400

    fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,7 @@ const Footer = () => (
       href='https://github.com/rajshekhar26/cleanfolio'
       className='link footer__link'
     >
-<<<<<<< HEAD
-      ChannyCodez
-=======
-      
->>>>>>> 4cd205ed06676e84a7cdce542b99fa5238e3e055
+      Created By Raj Shekhar
     </a>
   </footer>
 )
